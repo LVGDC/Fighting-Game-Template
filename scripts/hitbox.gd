bc1aae98aa8cc3060a38727f25669893a7afe6f7
mukarramarif <mukarramarif03@gmail.com>
2025-11-09 21:20:01 -0500

    hitboxes work well enough

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -3,5 +3,7 @@ class_name HitBox
 extends Area2D
 
 func _ready():
+	print("active")
 	collision_layer = 2
 	collision_mask = 0
+	print(collision_layer, collision_mask)
--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -4,3 +4,11 @@ extends Area2D
 func _ready():
 	collision_layer = 0
 	collision_mask = 2
+	print(collision_layer, collision_mask)
+	self.area_entered.connect(on_area_entered)
+func on_area_entered(hit_box: Area2D)-> void:
+	print("enter")
+	print(hit_box)
+	if hit_box is Hitbox:
+		var hit = hit_box as Hitbox
+		print("hit by ", hit)
